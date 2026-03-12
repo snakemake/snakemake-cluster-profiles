70c2c606872dd9cc112ad0ec6e491da821d423a0
lukavom <58477832+lukavom@users.noreply.github.com>
2026-03-12 14:40:02 +0100

    Change path for Snakemake tarball in job script
    Updated path for Snakemake tarball extraction.

--- profiles/htcondor-baf/condor-jobscript.sh
+++ profiles/htcondor-baf/condor-jobscript.sh
@@ -3,7 +3,7 @@
 export HOME=$(pwd)
 . /etc/profile
 ls -l
-tar --extract -z --file /cephfs/user/s6luvomb/venvs/venv_snakemake.tar.gz
+tar --extract -z --file <PATH TO SNAKEMAKE TARBALL ON SHARED FS>/venv_snakemake.tar.gz
 module load miniforge/24.11.2-1-py312
 . venv_snakemake/bin/activate
 
